4f23b8801f5dbe0a7171d70dd3c5f6c24aee8b60
Burhan <taskesenburhan@gmail.com>
2022-09-03 15:12:24 +0300

    iterable düzeltme

--- FMSS/src/thirdweek/IterableString.java
+++ FMSS/src/thirdweek/IterableString.java
@@ -3,46 +3,6 @@ package thirdweek;
 import java.util.Iterator;
 import java.util.function.Consumer;
 
-//public class IterableString implements Iterable<Character> {
-//    String value_;
-//
-//    IterableString(String value){
-//        value_ = value;
-//    }
-//
-//    @Override
-//    public Iterator<Character> iterator() {
-//        return new Iterator<>() {
-//            int iterator = 0;
-//            @Override
-//            public boolean hasNext() {
-//                if(iterator < value_.length())
-//                    return true;
-//                else {
-//                    return false;
-//                }
-//            }
-//
-//            @Override
-//            public Character next() {
-//                return value_.charAt(iterator++);
-//            }
-//        };
-//    }
-//
-//    public static void main(String[] args) {
-//        IterableString characters = new IterableString("Merhaba");
-//        for (Character character : characters) {
-//            System.out.println(character);
-//        }
-//        for (Character character : characters) {
-//            System.out.println(character);
-//        }
-//
-//
-//    }
-//}
-
 public class IterableString implements Iterable<Character> {
     String value_;
 
@@ -65,7 +25,7 @@ public class IterableString implements Iterable<Character> {
 
             @Override
             public Character next() {
-                return value_.toCharArray()[iterator++];
+                return value_.charAt(iterator++);
             }
         };
     }
@@ -78,5 +38,45 @@ public class IterableString implements Iterable<Character> {
         for (Character character : characters) {
             System.out.println(character);
         }
+
+
     }
 }
+
+//public class IterableString implements Iterable<Character> {
+//    String value_;
+//
+//    IterableString(String value){
+//        value_ = value;
+//    }
+//
+//    @Override
+//    public Iterator<Character> iterator() {
+//        return new Iterator<>() {
+//            int iterator = 0;
+//            @Override
+//            public boolean hasNext() {
+//                if(iterator < value_.length())
+//                    return true;
+//                else {
+//                    return false;
+//                }
+//            }
+//
+//            @Override
+//            public Character next() {
+//                return value_.toCharArray()[iterator++];
+//            }
+//        };
+//    }
+//
+//    public static void main(String[] args) {
+//        IterableString characters = new IterableString("Merhaba");
+//        for (Character character : characters) {
+//            System.out.println(character);
+//        }
+//        for (Character character : characters) {
+//            System.out.println(character);
+//        }
+//    }
+//}
