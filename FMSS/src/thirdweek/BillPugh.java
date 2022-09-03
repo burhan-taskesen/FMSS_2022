4da9c0b75c2817acce87e94f018e6e678e96601d
Burhan <taskesenburhan@gmail.com>
2022-09-03 22:32:44 +0300

    singleton

---
+++ FMSS/src/thirdweek/BillPugh.java
@@ -0,0 +1,23 @@
+package thirdweek;
+
+import java.math.BigInteger;
+
+public class BillPugh {
+    private BillPugh(){}
+
+    public static class SingletonHelper{
+        private final static BillPugh instance = new BillPugh();
+    }
+
+    public static BillPugh getInstance(){
+        return SingletonHelper.instance;
+    }
+
+    public static void main(String[] args) {
+        BillPugh instance1 = BillPugh.getInstance();
+
+        BillPugh instance2 = BillPugh.getInstance();
+
+        System.out.println(instance1 == instance2);
+    }
+}
